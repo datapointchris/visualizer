d80e95b8fc80665232cfc0d5f58efd735263530e
Chris Birch <chrisbirch@live.com>
2020-12-28 23:37:45 -0500

    bug: Update setup to include dependencies
    setup.py now includes dependencies for visualizer
    Update __init__ to import Visualizer
    Add requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
     description="Visualizer for data",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
     url="https://github.com/datapointchris/visualizer",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -18,5 +19,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=["numpy",
+                      "pandas",
+                      "wordcloud",
+                      "matplotlib",
+                      "seaborn",
+                      "pillow",
+                      "sklearn"],
     python_requires='>=3.6',
 )
